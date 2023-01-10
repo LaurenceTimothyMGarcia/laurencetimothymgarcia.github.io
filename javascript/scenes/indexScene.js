2d90f06b6c71852e577149125b5873dc12858198
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-01-10 15:57:48 -0800

    desk added for testing

--- javascript/scenes/indexScene.js
+++ javascript/scenes/indexScene.js
@@ -3,6 +3,9 @@
 //Import THREE JS 
 import * as THREE from 'three';
 
+//Loading models
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 //Font and text loaders and text geometry addon
 import { TTFLoader } from 'three/addons/loaders/TTFLoader';
 import { FontLoader } from 'three/addons/loaders/FontLoader';
@@ -33,8 +36,29 @@ renderer.render(scene, camera);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(ambientLight);
 
+const light = new THREE.PointLight(0xffffff, 10, 100);
+light.position.set(0, 5, 10);
+scene.add(light);
+
 //Control 3d scene
 const controls = new OrbitControls(camera, renderer.domElement);
+
+
+//Loader for 3d models
+const modelLoader = new GLTFLoader();
+
+//Desk
+//Test atm
+modelLoader.load( '../../models/Desk.glb', function ( gltf ) {
+
+    const deskMesh = gltf.scene.children[0];
+
+    scene.add(deskMesh);
+
+}, undefined, function ( error ) {
+    console.error( error );
+});
+
 
 
 //adding my name as text
