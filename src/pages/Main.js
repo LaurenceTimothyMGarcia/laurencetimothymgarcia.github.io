0f2abc889b46705f1b019f16d41b23648b5910e1
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2023-12-30 07:50:13 -0800

    work exp desktop

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Box } from '@chakra-ui/react'
 import AboutMeDesktop from './Main/AboutMeDesktop';
 import PortfolioDesktop from './Main/PortfolioDesktop';
+import WorkExpDesktop from './Main/WorkExpDesktop';
 
 function Main() {
   return(
     <Box>
       <AboutMeDesktop/>
+      <WorkExpDesktop/>
       <PortfolioDesktop/>
     </Box>
   )
