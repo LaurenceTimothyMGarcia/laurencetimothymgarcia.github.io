36fbb5f84e7af83aa2cf0f11bc88ddbc251a9d6f
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-02 20:57:45 -0800

    can now navigate menus

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
-import { Box, Flex, HStack, Heading, Spacer, StackDivider } from "@chakra-ui/layout";
+import { Box, Flex, HStack, Heading, Spacer, StackDivider, Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
 import React from "react";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return(
@@ -10,16 +11,24 @@ function NavBar() {
         alignItems='center'>
 
         <Box>
-          <Heading>Laurence Timothy M. Garcia</Heading>
+        <Link to='/'><Heading>Laurence Timothy M. Garcia</Heading></Link>
         </Box>
 
         <Spacer/>
 
         <HStack
           divider={<StackDivider borderColor='gray.200'/>}>
-          <Heading>About</Heading>
-          <Heading>Portfolios</Heading>
-          <Heading>Socials</Heading>
+          <Link to='/'><Heading>About</Heading></Link>
+
+          <Menu isLazy>
+            <MenuButton><Heading>Portfolios</Heading></MenuButton>
+            <MenuList>
+              <MenuItem><Link to='/swe-portfolio'>Software Engineering Portfolio</Link></MenuItem>
+              <MenuItem><Link to='/gamedev-portfolio'>Game Development Portfolio</Link></MenuItem>
+            </MenuList>
+          </Menu>
+          
+          <Button><Heading>Socials</Heading></Button>
         </HStack>
 
       </Flex>
