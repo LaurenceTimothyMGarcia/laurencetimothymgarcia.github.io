638cf7afaf2de6f13fa46ca17ee81e808bc48a34
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-04 10:21:43 -0800

    started ds card

--- src/pages/GameDevPortfolio/GameDevProjectCard.js
+++ src/pages/GameDevPortfolio/GameDevProjectCard.js
@@ -1,23 +1,32 @@
-import { Box, Center, Card, CardBody, CardFooter, CardHeader, Heading } from '@chakra-ui/react';
+import { Box, Heading, Image } from '@chakra-ui/react';
 import React from 'react';
 
 // Look like a ds screen
 
 function GameDevProjectCard() {
   return (
-    <Card bg={'green'}>
-      <CardHeader ml={'5%'} mr={'5%'} mt={'2%'} bg={'white'}>
-        <Center>
-          <Heading>Gunslinger Hilda</Heading>
-        </Center>
-      </CardHeader>
-
-      <CardBody>
-        
-      </CardBody>
-
-      <CardFooter></CardFooter>
-    </Card>
+    <Box 
+      bg={'green'} 
+      align={'center'}
+      pt={'1%'}
+      pb={'1%'}
+    >
+      <Heading
+        bg={'white'}
+        maxW={'90%'}
+        pt={'1%'}
+        pb={'1%'}
+        textColor={'black'}
+      >
+        Gunslinger Hilda
+      </Heading>
+      
+      <Image 
+        src='/logos/ProjectIcons/GunslingerHilda.png'
+        minW={'90%'}
+        maxW={'90%'}
+      />
+    </Box>
   )
 }
 
