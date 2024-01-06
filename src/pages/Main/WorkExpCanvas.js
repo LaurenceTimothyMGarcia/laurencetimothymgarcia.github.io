b8fbf30e8b25cc70961193c2dfc902d191ac65d7
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-05 17:41:43 -0800

    nickelodn room finished

--- src/pages/Main/WorkExpCanvas.js
+++ src/pages/Main/WorkExpCanvas.js
@@ -43,8 +43,8 @@ function WorkExpCanvas() {
       </Suspense>
 
       <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[25, 25, 25]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      {/* <spotLight position={[25, 25, 25]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
 
       <OrbitControls/>
     </Canvas>
