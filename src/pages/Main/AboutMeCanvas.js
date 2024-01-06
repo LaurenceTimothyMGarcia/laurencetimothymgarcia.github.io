136152813adf8839a5c2345a34d0bcbab3e7d7be
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-06 10:55:16 -0800

    added new room about me

--- src/pages/Main/AboutMeCanvas.js
+++ src/pages/Main/AboutMeCanvas.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Canvas, useFrame, useThree } from "@react-three/fiber"
 import { Suspense } from 'react'
 import { OrbitControls } from "@react-three/drei"
-import AboutMeRoom from '../../models/AboutMeRoom'
+import AboutMeRoom from '../../models/AboutMeRoomTextured'
 
 function CamSettings() {
   const { camera } = useThree();
@@ -29,8 +29,8 @@ function AboutMeCanvas() {
       </Suspense>
 
       <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[25, 25, 25]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      {/* <spotLight position={[25, 25, 25]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
       <OrbitControls/>
     </Canvas>
   )
