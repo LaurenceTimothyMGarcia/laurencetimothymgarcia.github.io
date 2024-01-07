2a8a5acc050258e4ed580dcf3b84bd79cc277cb9
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-07 11:31:45 -0800

    removed all for this page

--- src/data/Introduction.js
+++
@@ -1,5 +0,0 @@
-// Introduction
-
-const Intro = "Hi there! I am Laurence Garcia, a newly graduated Computer Science major from Cal Poly Pomona. I recently interned at Nickelodeon Animation Studios, where I developed production-enhancing tools that help assist the various teams. I was also the President and Secretary of the CPP Game Development Club, where I brought together a community of students to learn more about the game development industry, through guest speakers and club projects. I am expanding my skills in programming languages and frameworks, such as Java, C#, Python, React, and many more. My goal is to continue growing my skill set in the fields of Software Engineering, Game Development, and Computer Graphics."
-
-export default Intro;
