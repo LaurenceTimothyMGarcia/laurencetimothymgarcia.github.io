ab00303cc5ea76ac70dd031eb18b645284119139
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-07 08:51:13 -0800

    added comments to color

--- src/theme/Color.js
+++ src/theme/Color.js
@@ -1,5 +1,10 @@
 import { extendTheme } from "@chakra-ui/react";
 
+// Text + Background: Neutral Colors
+// Primary for main CTAs and Sections
+// Secondary for less ipmortant buttons and info cards
+// Accent color appears in images, highlights, hyperlinks, boxes, cards, etc.
+
 const colors = {
   bg: "#010A25",
   primary: "#7F9FFA",
