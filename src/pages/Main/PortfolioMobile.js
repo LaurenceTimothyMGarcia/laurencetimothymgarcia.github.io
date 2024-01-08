8b4dfaf893912e60db02564a3a08cbbb4e4a741b
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-08 12:54:48 -0800

    portfolio mobile version done

--- src/pages/Main/PortfolioMobile.js
+++ src/pages/Main/PortfolioMobile.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import colors from '../../theme/Color';
+import { Link } from "react-router-dom";
+import { Box, VStack, Center, Heading } from '@chakra-ui/react';
+import SWECanvas from './SWECanvas';
+import GameDevCanvas from './GameDevCanvas';
 
 // Portfolio section mobile
 
 function PortfolioMobile() {
   return(
-    <Box>
-      
+    <Box
+      mt={'1rem'}
+      mb={'2rem'}
+    >
+      <VStack>
+        <Center><Heading mb={'1rem'} mt={'3rem'} color={colors.primary}>Project Portfolios</Heading></Center>
+
+        <Box>
+          <Center><Heading size={'md'} color={colors.secondary}><Link to='/swe-portfolio'>Software Engineering Portfolio</Link></Heading></Center>
+
+          <Link to='/swe-portfolio'>
+            <SWECanvas/>
+          </Link>
+        </Box>
+
+        <Box>
+          <Center><Heading size={'md'} color={colors.secondary}><Link to='/gamedev-portfolio'>Game Development Portfolio</Link></Heading></Center>
+
+          <Link to='/gamedev-portfolio'>
+            <GameDevCanvas/>
+          </Link>
+        </Box>
+
+      </VStack>
     </Box>
   )
 }
