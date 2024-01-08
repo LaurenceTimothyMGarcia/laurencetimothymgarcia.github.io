05b4be9694449962004cf77270b73ad68b9e6eab
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-08 12:24:27 -0800

    About Me Mobile Done

--- src/pages/Main/AboutMeDesktop.js
+++ src/pages/Main/AboutMeDesktop.js
@@ -53,22 +53,6 @@ function AboutMeDesktop() {
         // bg={'tomato'}
       >
         <Education />
-        {/* <Center><Heading color={colors.secondary}>Education</Heading></Center>
-        <Center><Heading size={'md'}>Computer Science B.S</Heading></Center>
-        <Center><Heading size={'md'}>Cal Poly Pomona</Heading></Center>
-
-        <Center>
-          <UnorderedList>
-            <ListItem>GPA: 3.82</ListItem>
-            <ListItem>Summa Cum Laude</ListItem>
-            <ListItem>Dean's & President's List: All Semesters</ListItem>
-            <ListItem>CPP Game Development Club</ListItem>
-            <UnorderedList>
-              <ListItem>Secretary (2021-2022)</ListItem>
-              <ListItem>President (2022-2023)</ListItem>
-            </UnorderedList>
-          </UnorderedList>
-        </Center> */}
       </GridItem>
 
       <GridItem 
--- src/pages/Main/AboutMeMobile.js
+++ src/pages/Main/AboutMeMobile.js
@@ -1,22 +1,38 @@
 import React from 'react';
-import { Box, VStack } from '@chakra-ui/react';
+import { Box, VStack, Center, Heading } from '@chakra-ui/react';
 import AboutMeCanvas from './AboutMeCanvas';
 import SkillDisplay from '../../components/SkillDisplay';
 import SkillLanguages from '../../data/SkillLanguages';
 import SkillTools from '../../data/SkillTools';
 import IntroSection from './IntroSection';
 import Education from './Education';
+import colors from '../../theme/Color';
 
 // About Me Mobile Page
 
 function AboutMeMobile() {
   return(
-    <Box>
+    <Box 
+      mt={'1rem'}
+      mb={'2rem'}
+    >
       <VStack >
+        {/* 3D Models */}
         <AboutMeCanvas />
+
+        {/* Intro Segment */}
         <IntroSection />
 
+        {/* Education Segment */}
         <Education />
+
+        {/* Shows off skills */}
+        <Box>
+          <Center><Heading size={'lg'} color={colors.secondary}>Skills</Heading></Center>
+          <SkillDisplay sectionTitle={'Programming Languages'} skillSet={SkillLanguages}/>
+          <SkillDisplay sectionTitle={'Tools'} skillSet={SkillTools}/>
+        </Box>
+        
       </VStack>
     </Box>
   )
