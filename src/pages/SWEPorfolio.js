4c749685531bdf9f24263bd4e1bc953aee5df767
Laurence Timothy Garcia <45772910+LaurenceTimothyMGarcia@users.noreply.github.com>
2024-01-08 13:06:27 -0800

    swe portfolio mobile

--- src/pages/SWEPorfolio.js
+++ src/pages/SWEPorfolio.js
@@ -5,6 +5,16 @@ import SocialIcon from '../components/SocialIcon';
 import colors from '../theme/Color';
 
 function SWEPorfolio() {
+
+  let display = DesktopVer();
+  // display = MobileVer();
+
+  return (
+    display
+  )
+}
+
+function DesktopVer() {
   return (
     <Box>
       <Center
@@ -12,6 +22,35 @@ function SWEPorfolio() {
       >
         <Heading
           color={colors.primary}
+        >
+          Software Engineering Portfolio
+        </Heading>
+      </Center>
+
+      <Center mb={'1rem'}>
+        <Box display={'flex'} justifyContent="center" alignItems="center">
+          <SocialIcon
+            link={'https://github.com/LaurenceTimothyMGarcia'} 
+            img={'/logos/SocialLogos/GithubLogoPurple.png'} 
+          />
+        </Box>
+      </Center>
+      
+
+      <SWEProjectDisplay />
+    </Box>
+  )
+}
+
+function MobileVer() {
+  return (
+    <Box>
+      <Center
+        p={'2vh'}
+      >
+        <Heading
+          color={colors.primary}
+          size={'md'}
         >
           Software Engineering Portfolio
         </Heading>
